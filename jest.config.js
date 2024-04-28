7cdae82c07d0c1375351b47e299de5324669a3b9
e1327906 <e1327906@u.nus.edu>
2024-04-28 18:57:31 +0800

    disable sonarcloud

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+    '^.+\\.tsx?$': 'babel-jest',
   },
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
